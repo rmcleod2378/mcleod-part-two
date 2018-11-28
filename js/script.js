4f2d08df3183d8959cb1cf98703f7639b8648bb8
Renee McLeod <[mcleod2378@gmail.com]>
2018-11-28 17:56:16 -0500

    2nd attempt

--- js/script.js
+++ js/script.js
@@ -3,50 +3,47 @@ two conditions: the temperature and the event type. This information will be
 gathered through prompts.
   * (Renee McLeod)
  * (November 26, 2018)*/
- 
-imput java.util.Scanner';
-public class WhatToWear; {
-    public static void main(String[] args) {
-    //Create a Scanner
-    Scanner input = new Scanner(System.in);
-    
-    //Print opening statement
-    System.out.println("What should I wear?");
-    
-    //Print Event Type
-    System.out.println("What type of event is this?");
-    
-    //Print Temperature Outside
-    System.out.println("What is the temperature outside?");
-     
-    //Calculations and Variables
-    var event = prompt("Enter the type of event");
-    var temp = prompt("Enter the temp from outside");
-    	
-    
-function getEvent(clothing); {
-	if (casual) {
-	document.write ("wear something comfy.");
+
+
+//Calculations and Variables
+var event = prompt("Enter the type of event");
+var temp = prompt("Enter the temp from outside");
+var result1;
+var result2;
+var result;
+
+
+function getEvent(clothing) {
+	if (clothing === "casual") {
+	result1 = "should wear something comfy,";
+  return result1;
 	}
-	else if (semi-formal) {
-	document.write ("wear a polo.");
+	else if (clothing === "semi-formal") {
+	result1 = "should wear a polo,";
+  return result1;
 	}
-	else if (formal){
-	document.write ("wear a suit");
+	else if (clothing === "formal"){
+	result1 =  "should wear a suit,";
+  return result1;
 	}
-}  
-function getTemp(weather); {
+}
+function getTemp(temp) {
 		if (temp >= 70) {
-		document.write ("No jacket will be needed.");
+		result2 = "no jacket will be needed.";
+    return result2;
 		}
-		else if (temp >= 54) {
-		document.write ("Bring a jacket.");
+		else if (temp >= 54 && temp < 70) {
+		result2 = "bring a jacket.";
+    return result2;
 		}
 		else if (temp <= 54){
-		document.write ("Bring a coat.");
+		result2 = "bring a coat.";
+    return result2;
 		}
-		
+
 }
-	
-	getEvent(clothing);
-	getTemp(weather);
+getEvent(event);
+getTemp(temp);
+
+result = `Since it is ${temp} and you are going to a ${event} event, you ${result1} and ${result2}`;
+console.log(result);
